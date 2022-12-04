6854144a8976d14e8a624bc06380eaa310097cd9
reffyll <rdm008@yandex.ru>
2022-12-04 15:23:47 +0500

    lr2

--- src/timus/task1293.java
+++ src/timus/task1293.java
@@ -3,7 +3,7 @@ package timus;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class timus1 {
+public class task1293 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
